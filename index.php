95e1407dab9418f8a1b8171a7a7ef6b1f3e52ef7
Sebastian <brotrap007@gmail.com>
2022-04-21 18:53:55 -0300

    first commit

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+    <br>
+    <h1> ashha </h1>
+    <br>
+    <b> Hola Mundo </b>
+    <br>
+
+    <?php
+
+        $nombre_de_la_variable='valor'
+        for($i = 0; $i < 10; $i++) {
+            echo "<b> Hola mundo </b>" . "<br>";
+            echo "<b> Programando en php <b>" . "<br>";
+        }
+    ?>
+
+</body>
+</html>
